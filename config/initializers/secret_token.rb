11e8ceb50d6bf314a91c652ee8b736c92284209d
gelegele <ryo@ryo-macbook.local>
2012-11-27 11:21:12 +0900

    bootstrapの最初の画面

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsbs::Application.config.secret_token = '6d81ffc4b1f980d3cb9e403518a166a31adcfa1055b6ba7469f94ff718d41c66e244928bee2d813a97bd5bb345ee7885598dba4e03bcb7d5230603eafa62ded4'
